e469c0dd70c2944d56f4a431987f9314f6f38f64
mohammadmujtaba0 <mohammadmujtaba216@gmail.com>
2024-05-14 20:13:51 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todoQ1",
+      type: "Input",
+      message: "What do you want to add in your Todos?",
+    },
+
+    {
+      name: "todoQ2",
+      type: "confirm",
+      message: "Do you want to add more?",
+      default: "true",
+    },
+  ]);
+  todos.push(addTask.todoQ1);
+   condition = addTask.todoQ2
+  console.log(todos);
+}
